6ce5f5b3d9fc100bcdf8c9fa463478b2b3b43285
icewolf999 <lukacormier@gmail.com>
2023-03-27 11:51:02 +0300

    change1.1

--- mouvement.py
+++ mouvement.py
@@ -1,1 +1,18 @@
-print(move)
+class Player():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        velocity_x = 0
+        velocity_y = 0
+        anim_forward = []
+        anim_backward = []
+        anim_jump = []
+    def left():
+        velocity_x -= 1
+    def right():
+        velocity_x += 1
+    def up():
+        velocity_y += 1
+    def down():
+        velocity_y -= 1
+
