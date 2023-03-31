492aab35406cbd49920fbfea3e3fc7f312d950ee
icewolf999 <lukacormier@gmail.com>
2023-03-31 17:35:40 +0300

    hhhhhhh

--- enemy.py
+++ enemy.py
@@ -1,3 +1,5 @@
+import pygments
+from pyparsing import White
 import pygame 
 pygame.init()
 
@@ -5,14 +7,75 @@ running = True
 
 class rat():
     def __init__(self, x, y):
+        super().__init__()
+        self.image = pygments.Surface((50, 50))
+        self.image.fill(White)
         self.x = x
         self.y = y
-    move = False
+        hp = 40
+        self.velocity_x = 5
+        self.velocity_y = 5
+        
+
     while running:
         anim_forward_ratn = []
         anim_backward_ratn = []
         anim_up_ratn = []
         anim_down_ratn = []
+
+
+
+class ratb():
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygments.Surface((50, 50))
+        self.image.fill(White)
+        self.x = x
+        self.y = y
+        hp = 40
+        self.velocity_x = 5
+        self.velocity_y = 5
+        
+
+    while running:
+        anim_forward_ratb = []
+        anim_backward_ratb = []
+        anim_up_ratb = []
+        anim_down_ratb = []
+
+
+class monkey():
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygments.Surface((50, 50))
+        self.image.fill(White)
+        self.x = x
+        self.y = y
+        hp = 40
+        self.velocity_x = 5
+        self.velocity_y = 5
+        
+
+    while running:
+        anim_forward_monkey = []
+        anim_backward_monkey = []
+        anim_up_monkey = []
+        anim_down_monkey = []
+
+class dragon():
+    def __init__(self, x, y):
+         super().__init__()
+         self.image = pygments.Surface((50, 50))
+         self.image.fill(White)
+         self.x = x
+         self.y = y
+         hp = 40
+         self.velocity_x = 5
+         self.velocity_y = 5
+
+
+
+
            
         
     
